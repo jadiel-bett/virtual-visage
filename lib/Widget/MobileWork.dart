d1df6577a76cfcd9bb8be09cfcebe512e918401c
Jadiel Bett <jadielbett@gmail.com>
2024-05-13 12:51:47 +0300

    Fix stack overflow error

--- lib/Widget/MobileWork.dart
+++ lib/Widget/MobileWork.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MobileWork extends StatefulWidget {
   @override
@@ -14,75 +13,6 @@ class _MobileWorkState extends State<MobileWork> {
       constraints: BoxConstraints.tightFor(
         width: size.width,
         height: size.height * 1.7,
-      ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: size.height * 0.07,
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Container(
-                    height: size.height * 1.2,
-                    //color: Colors.indigo,
-                    child: Stack(
-                      children: [
-                        Center(
-                          child: VerticalDivider(
-                            color: Color(0xff64FFDA),
-                            thickness: 1.75,
-                            width: 10,
-                            indent: 10,
-                            endIndent: 10,
-                          ),
-                        ),
-                        Container(
-                          child: Center(
-                              child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              CircleAvatar(
-                                backgroundColor: Colors.pink,
-                                child: FaIcon(FontAwesomeIcons.houseLaptop,
-                                    color: Colors.white),
-                              ),
-                              CircleAvatar(
-                                backgroundColor: Colors.red,
-                                child: FaIcon(FontAwesomeIcons.freeCodeCamp,
-                                    color: Colors.white),
-                              ),
-                              CircleAvatar(
-                                backgroundColor: Colors.brown,
-                                child: FaIcon(FontAwesomeIcons.laptopCode,
-                                    color: Colors.white),
-                              ),
-                              CircleAvatar(
-                                backgroundColor: Colors.deepOrange,
-                                child: FaIcon(FontAwesomeIcons.mugSaucer,
-                                    color: Colors.white),
-                              ),
-                              CircleAvatar(
-                                backgroundColor: Colors.deepPurple,
-                                child: FaIcon(FontAwesomeIcons.dev,
-                                    color: Colors.white),
-                              ),
-                            ],
-                          )),
-                        )
-                      ],
-                    )),
-              ),
-              Expanded(
-                  flex: 4,
-                  child: Container(
-                    height: size.height * 1.7,
-                    child: MobileWork(),
-                  ))
-            ],
-          )
-        ],
       ),
     );
   }
