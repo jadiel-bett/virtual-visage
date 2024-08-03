a47e7527fc2397781d8fffbef9839d9d4e778989
Jadiel Bett <jadielbett@gmail.com>
2024-08-03 15:15:48 +0300

    Update build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -62,7 +62,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f24ce602ffd904a794c5b847504e70dc
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.ico": "958ca16c346fbfb2648e04f019bcac95",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f416d3d2f2d785e6926c4f3790304488",
+"flutter_bootstrap.js": "95982d79104e1f1b21b69547222a4be9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
